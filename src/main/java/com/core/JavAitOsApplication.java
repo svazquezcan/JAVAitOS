f6f49962a255437e5009b16244577f97376f3930
Alberto Marín <amarinmartinez@uoc.edu>
2021-03-06 11:20:55 +0100

    Inicio proyecto

---
+++ src/main/java/com/core/JavAitOsApplication.java
@@ -0,0 +1,13 @@
+package com.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavAitOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavAitOsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/core/JavAitOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavAitOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
